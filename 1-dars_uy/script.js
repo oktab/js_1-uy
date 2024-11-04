2111fafb48f41c5369a99e672b651902302f160e
oktab <163620755+oktab@users.noreply.github.com>
2024-11-04 18:24:10 +0300

    Add files via upload

---
+++ 1-dars_uy/script.js
@@ -0,0 +1,9 @@
+let ism = prompt("ismingizni kiriting")
+let fam = prompt("familyangizni kiriting")
+let fan = prompt("yoqtirgan faningizni kiriting")
+let joy = prompt("turara joyingizni kiriting")
+
+console.log(ism);
+console.log(fam);
+console.log(fan);
+console.log(joy);
